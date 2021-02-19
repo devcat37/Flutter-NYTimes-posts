cf3deb880f342714fde5792811e85998716fd5dd
devcat37 <egorka4ik@mail.ru>
2021-02-19 12:10:56 +0300

    Initial setup

---
+++ lib/internal/application.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NYTimes Posts',
+      theme: ThemeData(),
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nytimes/internal/application.dart';
+
+void main() {
+  runApp(Application());
+}
